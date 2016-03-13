b1c9f0ca3db0c23d2032ffa7d8c78acc3891fc92
Brian van de Boogaard <Brian.vandeBoogaard@Cerner.com>
2016-03-13 10:57:57 -0500

    Add @specs and docs to Blackjack.Cache

--- lib/blackjack/cache.ex
+++ lib/blackjack/cache.ex
@@ -1,31 +1,54 @@
 defmodule Blackjack.Cache do
-  @moduledoc false
+  @moduledoc """
+  Stores `Blackjack.Game.t` data for a single game to enable
+  game state to persist between process restarts.
+  """
 
   use GenServer
 
-  ## Api
+  # Api
 
+  @doc """
+  Start new `Blackjack.Cache` process with an initial state.
+  """
+  @spec start_link(Blackjack.Game.t) :: {:ok, pid}
   def start_link(state) do
     GenServer.start_link(__MODULE__, state)
   end
 
+  @doc """
+  Retrieve current state.
+  """
+  @spec get(pid) :: Blackjack.Game.t
   def get(pid) do
     GenServer.call(pid, {:get})
   end
 
+  @doc """
+  Replace the current state of the cache with the provided state.
+  """
+  @spec store(pid, Blackjack.Game.t) :: none
   def store(pid, state) do
     GenServer.cast(pid, {:store, state})
   end
 
+  @doc """
+  Replace current state with a new `Blackjack.Game.t`.
+  """
+  @spec clear(pid) :: none
   def clear(pid) do
     GenServer.cast(pid, {:clear})
   end
 
-  ## Callbacks
+  # Callbacks
+
+  ## Calls
 
   def handle_call({:get}, _from, state) do
     {:reply, state, state}
   end
+
+  ## Casts
 
   def handle_cast({:store, new_state}, _state) do
     {:noreply, new_state}
