537a2319be48c19396a6027092bf22175d204658
Brian van de Boogaard <Brian.vandeBoogaard@Cerner.com>
2016-03-13 11:11:04 -0500

    Add doc to Blackjack.Game

--- lib/blackjack/game.ex
+++ lib/blackjack/game.ex
@@ -1,57 +1,98 @@
 defmodule Blackjack.Game do
-  @moduledoc false
+  @moduledoc """
+  Main process that coordinates the Blackjack game.
 
-  defstruct players: [], deck: [%Blackjack.Card{}], cache: nil
+  Defines the struct `%Blackjack.Game{players: [], deck: [], cache: nil}`
+  used to maintain the state of the game.
+  """
+
+  defstruct players: [], deck: [], cache: nil
 
   use GenServer
   alias Blackjack.{Player, Deck, Game, Cache}
 
   @type t :: %Game{players: [pid], deck: [Blackjack.Card.t], cache: pid}
 
-  ## Api
+  # Api
 
+  @doc """
+  Starts a new `Blackjack.Game` process that will retrieve its initial
+  state from the procided `Blackjack.Cache` `pid`.
+  """
+  @spec start_link(pid) :: {:ok, pid}
   def start_link(cache) do
     GenServer.start_link(__MODULE__, cache)
   end
 
-  def register_player(pid, player) do
-    GenServer.cast(pid, {:register_player, player})
-  end
-
+  @doc """
+  Starts the game loop.
+  """
+  @spec play(pid) :: {:ok, :done}
   def play(pid) do
     GenServer.cast(pid, {:play})
   end
 
+  # Callbacks
+
+  @doc """
+  Retrieves the state from the provided cache `pid` to use as
+  the games initial state.
+
+  The provided cache `pid` is added to the initial
+  `Blackjack.Game.t`.
+  """
   def init(cache) do
     state = %Game{Cache.get(cache) | cache: cache}
     {:ok, state}
   end
 
-  ## Callbacks
-
+  @doc """
+  Starts the game.
+  """
   def handle_cast({:play}, game) do
     play_game(game)
   end
 
-  def handle_cast({:register_player, player}, game) do
-    {:noreply, %Game{game | players: game.players ++ [player]}}
-  end
-
+  @doc """
+  Updates the `Blackjack.Cache` with the current state before
+  the process is terminated.
+  """
   def terminate(_reason, state) do
     Cache.store(state.cache, state)
   end
 
-  ## Private
+  # Private
 
   defp play_game(game) do
     n_game = deal_game(game)
     game_loop(game.players, n_game)
   end
 
+  defp deal_game(game) do
+    n_game = Enum.reduce(game.players, game, fn (p, g) -> deal_card(p, g) end)
+    Enum.reduce(n_game.players, n_game, fn (p, g) -> deal_card(p, g) end)
+  end
+
+  defp deal_card(player, game) do
+    {:ok, card, new_deck} = Deck.draw(game.deck)
+    Player.take_card(player, card)
+    %Game{game | deck: new_deck}
+  end
+
   defp game_loop([], game), do: end_game(game)
   defp game_loop([player | rest], game) do
     n_game = player_move(player, game)
     game_loop(rest, n_game)
+  end
+
+  defp player_move(player, game) do
+    case Player.make_move(player) do
+      {:hit} ->
+        n_game = deal_card(player, game)
+        player_move(player, n_game)
+      {:stand} ->
+        game
+    end
   end
 
   defp end_game(game) do
@@ -67,26 +108,5 @@ defmodule Blackjack.Game do
     |> print_winner.()
 
     {:noreply, :done}
-  end
-
-  defp player_move(player, game) do
-    case Player.make_move(player) do
-      {:hit} ->
-        n_game = deal_card(player, game)
-        player_move(player, n_game)
-      {:stand} ->
-        game
-    end
-  end
-
-  defp deal_game(game) do
-    n_game = Enum.reduce(game.players, game, fn (p, g) -> deal_card(p, g) end)
-    Enum.reduce(n_game.players, n_game, fn (p, g) -> deal_card(p, g) end)
-  end
-
-  defp deal_card(player, game) do
-    {:ok, card, new_deck} = Deck.draw(game.deck)
-    Player.take_card(player, card)
-    %Game{game | deck: new_deck}
   end
 end
